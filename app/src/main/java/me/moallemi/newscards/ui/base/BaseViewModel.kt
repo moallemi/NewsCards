e3362e1702a212b1b18e11a6d33efa54b02f7ad8
Reza Moallemi <rm.allmails@gmail.com>
2019-06-10 11:07:44 +0430

    Add support for viewModelScope

--- app/src/main/java/me/moallemi/newscards/ui/base/BaseViewModel.kt
+++ app/src/main/java/me/moallemi/newscards/ui/base/BaseViewModel.kt
@@ -1,5 +1,15 @@
 package me.moallemi.newscards.ui.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-abstract class BaseViewModel : ViewModel()
+abstract class BaseViewModel : ViewModel() {
+
+    fun launchIO(block: suspend () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            block.invoke()
+        }
+    }
+}
