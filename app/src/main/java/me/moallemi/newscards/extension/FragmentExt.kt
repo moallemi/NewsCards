e85bcb3150ab2e5be92a4f644c26b4505bbc9e9f
moein <Moeinabdi77@gmail.com>
2020-12-06 11:15:53 +0330

    refactor code to the latest libs(Glide & life cycle)

--- app/src/main/java/me/moallemi/newscards/extension/FragmentExt.kt
+++ app/src/main/java/me/moallemi/newscards/extension/FragmentExt.kt
@@ -3,13 +3,12 @@ package me.moallemi.newscards.extension
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 
 inline fun <reified T : ViewModel> Fragment.createViewModel(
     factory: ViewModelProvider.Factory,
     body: T.() -> Unit = {}
 ): T {
-    val viewModel = ViewModelProviders.of(this, factory)[T::class.java]
+    val viewModel = ViewModelProvider(this, factory).get(T::class.java)
     viewModel.body()
     return viewModel
 }
--- app/src/main/java/me/moallemi/newscards/extension/ImageViewExt.kt
+++ app/src/main/java/me/moallemi/newscards/extension/ImageViewExt.kt
@@ -2,24 +2,26 @@ package me.moallemi.newscards.extension
 
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
-import me.moallemi.newscards.util.image.GlideApp
 
 fun ImageView.load(url: String?, placeHolder: Drawable? = null, roundedCorner: Int = 0) {
     url?.let {
-        GlideApp.with(this)
+        Glide.with(this)
             .load(url)
             .transition(DrawableTransitionOptions.withCrossFade())
-            .apply(RequestOptions().apply {
-                placeholder(placeHolder)
-                if (roundedCorner > 0) {
-                    transform(MultiTransformation(CenterCrop(), RoundedCorners(roundedCorner)))
+            .apply(
+                RequestOptions().apply {
+                    placeholder(placeHolder)
+                    if (roundedCorner > 0) {
+                        transform(MultiTransformation(CenterCrop(), RoundedCorners(roundedCorner)))
+                    }
                 }
-            })
+            )
             .into(this)
     }
 }
